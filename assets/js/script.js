e177529d108c084b2eaf5607a65e307d195478ac
EmlSn <eml.snzrmz@gmail.com>
2021-10-19 19:46:10 -0500

    Added line to explain a perfect number.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
-let divisibleNumbers = [];
+let divisors = [];
 const sum = (a, b) => a + b;
 
 function perfectNumber(number) {
   for (let i = 0; i <= number / 2; i++) {
     if (number % i == 0) {
-      divisibleNumbers.push(i);
+      divisors.push(i);
     }
   }
-  const sumDivisibleNumbers = divisibleNumbers.reduce(sum);
-  if (sumDivisibleNumbers !== 0 && sumDivisibleNumbers == number) {
-    console.log(`${number} is a perfect number.`);
+  const sumDivisors = divisors.reduce(sum);
+  if (sumDivisors !== 0 && sumDivisors == number) {
+    console.log(`${number} is a perfect number`);
+    console.log(`Since its positive divisors: ${divisors} sum up to ${number}`);
   } else {
     console.log(`${number} is not a perfect number, try another one.`);
   }
