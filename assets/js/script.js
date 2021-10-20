ced84e13ac2c4056e8ac271962017c6f604cf73a
EmlSn <eml.snzrmz@gmail.com>
2021-10-20 00:39:42 -0500

    Finished project.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,33 @@
-let divisors = [];
 const sum = (a, b) => a + b;
 
-function perfectNumber(number) {
-  for (let i = 0; i <= number / 2; i++) {
-    if (number % i == 0) {
-      divisors.push(i);
+function perfectNumber() {
+  // Brings the input from the user.
+  let number = document.getElementById("perfectInput");
+  number = number.value;
+
+  // Paragraph to display discount to the user.
+  let result = document.getElementById("result");
+  result.className = "showResult";
+
+  // Array for aliquot sum.
+  let divisors = [];
+
+  if (number <= 1) {
+    result.innerText = "Try with a something greater than one.";
+  } else {
+    // Original perfectNumber function.
+    for (let i = 0; i <= number / 2; i++) {
+      if (number % i == 0) {
+        divisors.push(i);
+      }
+    }
+    const sumDivisors = divisors.reduce(sum);
+    if (sumDivisors !== 0 && sumDivisors == number) {
+      result.innerText = `${number} is a perfect number
+    
+      Since its positive divisors: ${divisors} sum up to ${number}`;
+    } else {
+      result.innerText = `${number} is not a perfect number, try another one.`;
     }
   }
-  const sumDivisors = divisors.reduce(sum);
-  if (sumDivisors !== 0 && sumDivisors == number) {
-    console.log(`${number} is a perfect number`);
-    console.log(`Since its positive divisors: ${divisors} sum up to ${number}`);
-  } else {
-    console.log(`${number} is not a perfect number, try another one.`);
-  }
 }
-
-perfectNumber(28);
